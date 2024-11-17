99e361dd9a72bb6885ee76414c345876461395ae
Tai Luan Nguyen <luan.nguyen@workstream.is>
2024-11-16 17:39:22 -0800

    Update navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,32 @@
+import { createTheme } from "@mui/material/styles";
+import { useMemo} from "react";
+import { themeSettings } from "./theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "/src/scenes/navbar/index.tsx"; // @ is alias to /src
+import Dashboard from "/src/scenes/dashboard/index.tsx";
+
+
 function App() {
-  return <div className="app"></div>
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar /> 
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<div>prediction page</div>} />
+            </Routes>
+          </Box>
+  \      </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
